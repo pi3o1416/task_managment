b84816d3f5a33798a320aa566ac7c5272d819149
monir hossain <mmonir.m.29@gmail.com>
2022-06-10 08:50:10 +0600

    add Edit Task view

--- task/urls.py
+++ task/urls.py
@@ -6,4 +6,6 @@ app_name='task'
 urlpatterns = [
     path('create-task/', CreateTask.as_view(), name='create_task'),
     path('created-task-list/', CreatedTaskList.as_view(), name='created_task_list'),
+    path('edit-task/<int:id>/', EditTask.as_view(), name='edit_task'),
+
 ]
