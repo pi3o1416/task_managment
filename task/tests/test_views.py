5a5be1ecaa2671af49b73cfdb41d1ea463bda646
monir hossain <mmonir.m.29@gmail.com>
2022-06-11 01:21:27 +0600

    incomplete test for views

---
+++ task/tests/test_views.py
@@ -0,0 +1,25 @@
+
+from django.test import TestCase
+
+
+class TestCreateTask(TestCase):
+    pass
+
+class TestEditTask(TestCase):
+    pass
+
+
+class TestDeleteTask(TestCase):
+    pass
+
+
+class TestTaskQueue(TestCase):
+    pass
+
+
+class TestAssignTask(TestCase):
+    pass
+
+
+class TestCreatedTaskList(TestCase):
+    pass
